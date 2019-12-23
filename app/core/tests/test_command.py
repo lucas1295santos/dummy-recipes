497105fe7d508d1f670307f81a95fcc36bc113fc
lucas santos <lucas1295santos@gmail.com>
2019-12-23 14:29:42 -0300

    Creating a wait for db command

---
+++ app/core/tests/test_command.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test wait for db when db is available"""
+        # patch is a tool for mocking that override a func exposing
+        # useful stuff like call_count return_value side_efect
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test wait for db when it is unavailable and than become available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
